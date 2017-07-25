2cad2f9a97a0201c334e0301ded40393afd26554
Dave Brown <david.brown@nominum.com>
2017-07-25 11:39:17 -0500

    Let the exception tell us what it wants and retry

--- backfill.py
+++ backfill.py
@@ -4,6 +4,7 @@ import re
 import time as time_
 from log import IndigoLog, IndigoRecord, ReceivedEvent
 from influxdb import InfluxDBClient
+from influxdb.exceptions import InfluxDBClientError
 
 def temp(note):
     return note.split()[-1]
@@ -17,7 +18,104 @@ connection = InfluxDBClient( \
 
 il = IndigoLog()
 
+def valueOf(kk, vv):
+    if vv is None:
+        raise ValueError('nope')
+    elif isinstance(vv, bool):
+        return bool(vv)
+    elif isinstance(vv, dict):
+        raise ValueError('nope')
+    floatkeys = 'lastSuccessfulComm id lastChanged buttonGroupCount folderId brightness pressureMillibars'.split()
+    strkeys = 'PanicState.ambulance PanicState.duress LEDBypass hvacFanMode hvacFanModeIsAlwaysOn LEDMemory KeypadChime.enabled hvacFanIsOn hvacDehumidifierIsOn KeypadChime.disabled state.tripped humidityInputsAll state.open state.closed hvacCoolerIsOn hvacHeaterIsOn KeypadChime ArmedState.stay LastChangedTimer ArmedState ArmedState.away ArmedState.disarmed bypass.bypassed bypass.nobypass humidityInput1'.split()
+    intkeys = 'configured onOffState'.split()
+    boolkeys = 'notbool enabled'.split()
+    if kk in floatkeys:
+        return float(vv)
+    if kk in strkeys:
+        return str(vv)
+    if kk in intkeys:
+        return int(vv)
+    if kk in boolkeys:
+        return bool(vv)
+    if '.num' in kk:
+        return float(vv)
+    return vv
+
 def send(record):
+    # write existing debug info back - more data available
+    if record.notes.startswith('['):
+        oldjson = json.loads(record.notes)
+        newjson = {}
+        if 'name' not in oldjson[0].keys():
+            return
+        for kk in oldjson[0].keys():
+            vv = oldjson[0][kk]
+            try:
+                newjson[kk] = valueOf(kk, vv)
+            except ValueError:
+                pass
+        newjson['time'] = record.readtime().strftime('%Y-%m-%dT%H:%M:%SZ')
+
+        newjson = [newjson]
+        unsent = True
+        while unsent:
+            try:
+                connection.write_points(newjson, time_precision='s')
+                unsent = False
+            except InfluxDBClientError as e:
+                #print(str(e))
+                field = json.loads(e.content)['error'].split('"')[1]
+                measurement = json.loads(e.content)['error'].split('"')[3]
+                retry = json.loads(e.content)['error'].split('"')[4].split()[7]
+                if retry == 'integer':
+                    retry = 'int'
+                if retry == 'string':
+                    retry = 'str'
+                newcode = '%s(%s)' % (retry, str(newjson[0]['fields'][field]))
+                #print(newcode)
+                newjson[0]['fields'][field] = eval(newcode)
+
+    elif record.notes.startswith('{'):
+        oldjson = json.loads(record.notes)
+        newjson = {}
+        if 'name' not in oldjson.keys():
+            return
+        for kk in oldjson.keys():
+            vv = oldjson[kk]
+            try:
+                newjson[kk] = valueOf(kk, vv)
+            except ValueError:
+                pass
+        newtags = { 'name': newjson['name'] }
+        measurement = 'device_changes'
+        if 'thermostat' in newjson['name']:
+            measurement = 'thermostat_changes'
+        json_body=[
+            {
+                'measurement': measurement,
+                'time': record.readtime().strftime('%Y-%m-%dT%H:%M:%SZ'),
+                'tags' : newtags,
+                'fields':  newjson
+            }
+        ]
+        unsent = True
+        while unsent:
+            try:
+                connection.write_points(json_body, time_precision='s')
+                unsent = False
+            except InfluxDBClientError as e:
+                #print(str(e))
+                field = json.loads(e.content)['error'].split('"')[1]
+                measurement = json.loads(e.content)['error'].split('"')[3]
+                retry = json.loads(e.content)['error'].split('"')[4].split()[7]
+                if retry == 'integer':
+                    retry = 'int'
+                if retry == 'string':
+                    retry = 'str'
+                newcode = '%s(%s)' % (retry, str(json_body[0]['fields'][field]))
+                #print(newcode)
+                json_body[0]['fields'][field] = eval(newcode)
+
     # get an IndigoRecord, make up some json that mostly looks like an Indigo Device flattened to json
     try:
         event = record.readevent()
@@ -112,7 +210,10 @@ def send(record):
 for x in il.records():
     try:
         send(x)
-    except:
+    except json.JSONDecodeError as e:
+        pass
+    except Exception as e:
         print("skipping a line")
+        print(str(e))
         pass
 
